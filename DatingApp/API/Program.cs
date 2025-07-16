69af741e5d94d073686a27689373f9ac49d444fd
Scott Gerold <scott@futuresystemsinc.com>
2025-07-16 15:59:12 -0400

    Section 7

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200", "http://localhost:4200"));
